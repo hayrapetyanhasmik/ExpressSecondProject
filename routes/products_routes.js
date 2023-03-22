28c08437c0856b4776ab8a04907a584e5b4212ad
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2023-03-22 11:11:53 +0400

    Deleted midlware

--- routes/products_routes.js
+++ routes/products_routes.js
@@ -1,11 +1,10 @@
 const {add_products, get_all, get_by_id, update, deleted} = require('../controller/product_controller');
 const {authenticateTokenAdmin} = require('../JWT/jwt_authenticateAdmin');
-const {authenticateTokenUser} = require('../JWT/jwt_authenticateUser');
 
 function create_products_routes(app){
     app.post('/products/create', authenticateTokenAdmin, add_products)
-    app.get('/products', authenticateTokenUser, get_all)
-    app.get('/products/:id', authenticateTokenAdmin, get_by_id)
+    app.get('/products', get_all)
+    app.get('/products/:id', get_by_id)
     app.put('/products/update/:id', authenticateTokenAdmin, update)
     app.delete('/products/del/:id', authenticateTokenAdmin, deleted)
    
